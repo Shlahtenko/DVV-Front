928894d7a15c672d74377d0cfc351c436a00a88f
awenkoo <186723021+awenkoo@users.noreply.github.com>
2024-12-19 14:22:03 +0200

    feat: add alias pathes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/core/api'),
+      '@boot': path.resolve(__dirname, './src/ui/boot'),
+      '@components': path.resolve(__dirname, './src/ui/components'),
+      '@config': path.resolve(__dirname, './src/core/config'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@hooks': path.resolve(__dirname, './src/ui/hooks'),
+      '@pages': path.resolve(__dirname, './src/ui/pages'),
+      '@public': path.resolve(__dirname, './public'),
+      '@services': path.resolve(__dirname, './src/core/services'),
+      '@styles': path.resolve(__dirname, './src/ui/styles'),
+      '@utils': path.resolve(__dirname, './src/core/utils'),
+      '@views': path.resolve(__dirname, './src/ui/views'),
+    },
+  },
+});
