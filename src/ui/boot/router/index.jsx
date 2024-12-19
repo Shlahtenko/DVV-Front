97f2bf05e8120745b7a666b3e557fcc9db712710
awenkoo <186723021+awenkoo@users.noreply.github.com>
2024-12-19 15:11:34 +0200

    fix: routes

--- src/ui/boot/router/index.jsx
+++ src/ui/boot/router/index.jsx
@@ -1,8 +1,6 @@
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import Layout from '@components/Layout';
-
 import { ERROR, HOME, SETTINGS } from './routes';
 
 const Home = lazy(() => import('@pages/home'));
@@ -11,17 +9,11 @@ const Error = lazy(() => import('@pages/error'));
 
 const routes = [
   {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: HOME,
-        element: <Home />,
-      },
-      { path: SETTINGS, element: <Settings /> },
-      { path: ERROR, element: <Error /> },
-    ],
+    path: HOME,
+    element: <Home />,
   },
+  { path: SETTINGS, element: <Settings /> },
+  { path: ERROR, element: <Error /> },
 ];
 
 const router = createBrowserRouter(routes);
